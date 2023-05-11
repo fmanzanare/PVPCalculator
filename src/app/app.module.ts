387fd5d13500410b1de491bb251b74a040b88f5d
Fernando Manzanares Montes <98348993+fmanzanare@users.noreply.github.com>
2023-05-11 12:19:57 +0200

    sign-up initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     LoginComponent,
-    NavBarComponent
+    NavBarComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule
