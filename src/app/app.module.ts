19352573d76d396805eb218eb2e4fbf2beaabdb6
Fernando Manzanares Montes <98348993+fmanzanare@users.noreply.github.com>
2023-05-11 11:28:38 +0200

    Components organization changed:
    The app.component (main component) will host the UI and the other
    components will be called from that UI to be shown.
    The other components are shown in the right-bottom side of the screen.
    That section is called "main-section".

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
