a26fb80c08e1c19f1151a1c56b56cfe4bf988bd3
Fernando Manzanares Montes <98348993+fmanzanare@users.noreply.github.com>
2023-05-17 11:58:54 +0200

    navigation between login and sign-up finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { RoutingComponent } from './routing/routing.component';
+
+
+const appRoutes: Routes = [
+	{ path: 'login', component: LoginComponent},
+	{ path: 'sign-up', component: SignUpComponent},
+]
 
 @NgModule({
   declarations: [
@@ -13,12 +22,15 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     DashboardComponent,
     LoginComponent,
     NavBarComponent,
-    SignUpComponent
+    SignUpComponent,
+    RoutingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
