3b6e8e76ad593655042a67fee2b557bb2d56746a
Fernando Manzanares Montes <98348993+fmanzanare@users.noreply.github.com>
2023-05-17 12:00:24 +0200

    routing component deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { RoutingComponent } from './routing/routing.component';
 
 
 const appRoutes: Routes = [
@@ -23,7 +22,6 @@ const appRoutes: Routes = [
     LoginComponent,
     NavBarComponent,
     SignUpComponent,
-    RoutingComponent
   ],
   imports: [
     BrowserModule,
