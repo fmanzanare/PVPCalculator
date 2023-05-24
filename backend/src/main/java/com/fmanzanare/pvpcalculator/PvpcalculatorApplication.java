21fab606d48cf42ca66bb37d0ed96c272491e47c
Fernando Manzanares Montes <98348993+fmanzanare@users.noreply.github.com>
2023-05-24 11:38:54 +0200

    backend -Spring Boot project- add: using maven this time!

---
+++ backend/src/main/java/com/fmanzanare/pvpcalculator/PvpcalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.fmanzanare.pvpcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvpcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvpcalculatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fmanzanare/pvpcalculator/PvpcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmanzanare.pvpcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvpcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
